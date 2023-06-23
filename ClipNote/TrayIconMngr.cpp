d83384330b9da45415203d125e1bc51add1137b8
KJM <kjm99d@naver.com>
2023-06-23 17:55:55 +0900

    ✨ 트레이 메뉴 클릭 이벤트 개발
    - 메뉴 목록 보여주기 구현

--- ClipNote/TrayIconMngr.cpp
+++ ClipNote/TrayIconMngr.cpp
@@ -44,13 +44,46 @@ LRESULT CTrayIconMngr::OnTrayIcon(WPARAM wParam, LPARAM lParam)
     {
     case WM_RBUTTONDOWN: // 오른쪽 버튼 클릭
     {
+        CPoint pt;
+        GetCursorPos(&pt);
 
+        CMenu menu;
+        VERIFY(menu.LoadMenu(IDR_MENU2));
+
+        CMenu* pSubMenu = menu.GetSubMenu(0);
+        ASSERT(pSubMenu != NULL);
+
+        pSubMenu->TrackPopupMenu(TPM_LEFTALIGN | TPM_RIGHTBUTTON, pt.x, pt.y, this);
     }
+    break;
+
     case WM_LBUTTONDBLCLK: // 왼쪽 버튼 더블 클릭
+    {
         OutputDebugString(L"WM_LBUTTONDBLCLK \n");
         AfxGetMainWnd()->ShowWindow(SW_SHOW);
         AfxGetMainWnd()->SetForegroundWindow();
+    }
         break;
+
+
+    case WM_COMMAND:
+    {
+        switch (LOWORD(wParam))
+        {
+        case ID_TRAY_EXIT:
+            // 종료 버튼 클릭 시 어플리케이션 종료
+            if (MessageBox(_T("정말 종료하시겠습니까?"), _T("종료"), MB_YESNO | MB_ICONQUESTION) == IDYES)
+            {
+                PostMessage(WM_CLOSE);
+            }
+            break;
+
+        case ID_ENV_TOOL:
+            MessageBox(L"Message");
+            break;
+        }
+    }
+    break;
     }
 
     return 0;
--- ClipNote/resource.h
+++ ClipNote/resource.h
@@ -7,17 +7,24 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_CLIPNOTE_DIALOG             102
 #define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDR_MENU2                       131
 #define IDC_LIST1                       1000
 #define IDC_BUTTON1                     1002
 #define IDC_BUTTON2                     1003
 #define IDC_STATIC_Preview              1004
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_ENVTOOL                      32773
+#define ID_ENV_TOOL                     32774
+#define ID_TRAY_EXIT                    32775
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
